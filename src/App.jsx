08550d4ab27ad39af953182ff8ea0fdf275d75dc
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2023-07-29 18:01:01 +0000

    Twitter button styled

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App (){
+  return(
+    <section className="suggestionedUsers">
+      <header className="userContainer">
+        <img className="userImage" src="https://unavatar.io/midudev" alt="User avatar" />
+        <div className="userInfo">
+          <strong className="userName">Oscar Casado Lorenzo</strong>
+          <span className="userNick">@adolexente</span>
+        </div>
+      </header>
+      <aside>
+        <button className="followButton">Follow</button>
+      </aside>
+    </section>
+  )
+}
+  
