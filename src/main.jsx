27953848db2575ca22f29918d70688a8a7451671
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2023-07-29 17:16:24 +0000

    Rendering two elements with Fragment

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render('Hi world!')
+root.render(
+  <React.Fragment>
+    <button>Button 1</button>
+    <button>Button 2</button>
+  </React.Fragment>
+)
