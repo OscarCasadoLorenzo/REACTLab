e04f5c1e7ea7756ccf9ac4759c6c72a5de5a6a3e
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2023-08-01 01:31:52 +0200

    Conditional rendering with state & effect

--- 01-giphy-client/src/App.jsx
+++ 01-giphy-client/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import fetchGifts from './services/fetchGifts'
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [gifts, setGifts] = useState([])
+
+  useEffect(function(){
+    fetchGifts().then(gifts => setGifts(gifts))
+  },[])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='giftsContainer'>
+      {
+        gifts.map(singleGift =>
+          <img className='gift' src={singleGift}/>
+        )
+     }      
+    </div>
   )
 }
 
